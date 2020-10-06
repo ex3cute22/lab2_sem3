5332b5d59838aa82397101f0dc6c713f972c97d3
Ilya <viktorov.200@mail.ru>
2020-10-06 15:51:25 +0700

    AllFunction

--- lab2_sem3/lab2_sem3.cpp
+++ lab2_sem3/lab2_sem3.cpp
@@ -59,8 +59,12 @@ void Display(car* q) {
 }
 
 
-void Add(car* q) {
-    if (q);
+void Add(car* q, car* q1) {
+    int sum = q->price + q1->price;
+    if (sum > 350000) { strcpy_s(q->marka, "Ferrari"); strcpy_s(q->model, "F430"); q->engine = 5,0; q->power = 440; strcpy_s(q->transmission, "Автомат"); strcpy_s(q->color, "Красный"); q->year = 2018; q->price = 350000; }
+    else if (sum > 250000) { strcpy_s(q->marka, "Nissan"); strcpy_s(q->model, "Skyline"); q->engine = 3,0; q->power = 270; strcpy_s(q->transmission, "Автомат"); strcpy_s(q->color, "Серебристый"); q->year = 2004; q->price = 250000; }
+    else if (sum > 150000) { strcpy_s(q->marka, "ВАЗ"); strcpy_s(q->model, "2101"); q->engine = 1,0; q->power = 60; strcpy_s(q->transmission, "Механика"); strcpy_s(q->color, "Красный"); q->year = 1988; q->price = 150000; }
+    else { strcpy_s(q->marka, "ЗАЗ"); strcpy_s(q->model, "966"); q->engine = 0,8; q->power = 40; strcpy_s(q->transmission, "Механика"); strcpy_s(q->color, "Синий"); q->year = 1966; q->price = 0; }
 }
 
 
@@ -99,6 +103,7 @@ int main()
     }
     printf("\nКакой автомобиль вы хотите продать?\nУкажите порядковый номер: ");
     int num;
+    int num1;
     scanf_s("%d", &num);
     fflush;
     Sale(q + num - 1);
@@ -116,7 +121,32 @@ int main()
     fflush;
     printf("\nСтарый цвет: %s \tНовый цвет: ", q[num - 1].color);
     gets_s(color);
+    puts("\n");
     Paint(q + num - 1, color);
+    for (int i = 0; i < k; i++) {
+        Display(q + i);
+    }
+    system("pause");
+    system("cls");
+    for (int i = 0; i < k; i++) {
+        Display(q + i);
+    }
+    puts("\n");
+    if (k >= 2) {
+        printf("Какие 2 машины вы хотите обменять на другую(функция сложения)?\nУкажите порядковые номера.\nПервая машина: ");
+        scanf_s("%d", &num);
+        fflush;
+        printf("Вторая машина: ");
+        scanf_s("%d", &num1);
+        fflush;
+        Add(q + num - 1, q + num1 - 1);
+        for (int i = num1 - 1; i < k; i++) {
+            q[i] = q[i + 1];
+        }
+        k--;
+    }
+    else ("Обменять машины не получилось, так как их кол-во меньше 2");
+    puts("\n");
     for (int i = 0; i < k; i++) {
         Display(q + i);
     }
